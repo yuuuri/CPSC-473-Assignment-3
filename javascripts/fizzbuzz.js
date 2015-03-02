8ca75189bec6f10525774f887abc068791415710
yuuuri <yuuuri@yahoo.com>
2015-03-02 11:03:19 -0800

    changed to use array due to the problem not displaying web page correctly

--- javascripts/fizzbuzz.js
+++ javascripts/fizzbuzz.js
@@ -4,26 +4,39 @@
 var main = function() {
     "use strict";
 
-    var i;
+    var i, str_array[];
 
-    function fizzbuzz_1() {
-        $("body .first").append("<p>");
-        for (i=1; i<=100; i++) {
-            if ( (i%3 === 0 ) && (i%5 === 0) ) {
-                $("body .first").append("FizzBuzz ");
-            } else if ( i%3 === 0) {
-                $("body .first").append("Fizz ");
-            } else if ( i%5 === 0 ) {
-                $("body .first").append("Buzz ");
-            }
-            else {
-                $("body .first").append( i + " ")
-            }
-        }
-        $("body .first").append("</p><br>");
-    }
-    $("body .first").append("<p> FizzBuzz1: </p>");
-    fizzbuzz_1();
+	
+	function fizzbuzz_1(){
+        for (i = 1; i <= 100; i++) {
+			
+              // write FizzBuzz if divisible by 3 and 5  
+              if ((i%3 === 0) && (i%5 === 0)) {
+                    str_array.push("FizzBuzz");
+              // write Fizz if divisible by 3
+              } else if (i%3 === 0) {
+                    str_array.push("Fizz");
+              // write Buzz if divisble by 5
+              } else if (i%5 === 0) {
+                    str_array.push("Buzz");
+              // otherwise write i
+              } else {
+                    str_array.push(i);
+              }
+
+        } // end of for loop
+
+    //display output on webpage
+    str_array.forEach(function(element){
+        $("body .fizzbuzz_1").append($("<p>").text(element));
+    });
+
+  }// end of fizzbuzz_1
+   
+    //calling function to display number, Fizz, Buzz, or FizzBuzz
+   fizzbuzz_1();
+
+
 };
 $(document).ready(main);
 
