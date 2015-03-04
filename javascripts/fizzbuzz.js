27e10fa25ae8e7d050a6c6c19c00f064c548af5f
yuuuri <yuuuri@yahoo.com>
2015-03-03 21:05:45 -0800

    fizzbuzz_4 is finally displays on website correctly

--- javascripts/fizzbuzz.js
+++ javascripts/fizzbuzz.js
@@ -1,3 +1,7 @@
+//Yuri Van Steenburg
+//CS481 Assignment 3
+//I had my firends helped me for this homework.  Their names are Sarah Lee and Chris Danan.
+
 // Client-side code
 /* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
 
@@ -8,6 +12,7 @@ var main = function() {
 
 	  //writes number 1-100 except when divisible by 3, 5, and 3 & 5
     function fizzbuzz_1(){
+
         $("body .fizzbuzz_1").append("<p>FizzBuzz_1</p>");
         $("body .fizzbuzz_1").append("<p>");
 
@@ -35,6 +40,7 @@ var main = function() {
     
     //same function above except use parameter start and end
     function fizzbuzz_2(start, end) {
+
         $("body .fizzbuzz_2").append("<p>FizzBuzz_2</p>");
         $("body .fizzbuzz_2").append("<p>");
 
@@ -62,34 +68,65 @@ var main = function() {
     
     //same function above except use parameter start and end
     function fizzbuzz_3(arr) {
-            $("body .fizzbuzz_3").append("<p>FizzBuzz_3</p>");
-            $("body .fizzbuzz_3").append("<p>");
 
-            if (arr.length > 0) {
-                for (i = 0; i < arr.length; i++) {
-                      // write FizzBuzz if divisible by 3 and 5  
-                      if ( ( (arr[i]) %3 === 0) && ( (arr[i]) %5 === 0 ) ) {
-                            $("body .fizzbuzz_3").append("FizzBuzz ");
-                      // write Fizz if divisible by 3
-                      } else if ( (arr[i]) %3 === 0 ) {
-                            $("body .fizzbuzz_3").append("Fizz ");
-                      // write Buzz if divisble by 5
-                      } else if ( (arr[i]) %5 === 0 ) {
-                            $("body .fizzbuzz_3").append("Buzz ");
-                      // otherwise write i
-                      } else {
-                            $("body .fizzbuzz_3").append(" " + arr[i] + " ");
-                      }
+        $("body .fizzbuzz_3").append("<p>FizzBuzz_3</p>");
+        $("body .fizzbuzz_3").append("<p>");
 
-                } // end of for loop
-            } else {
-                alert("No element in array");
-                console.log("Error: No elements in array");
-            }
+        if (arr.length > 0) {
+
+            for (i = 0; i < arr.length; i++) {
+                  // write FizzBuzz if divisible by 3 and 5  
+                  if ( ( (arr[i]) %3 === 0) && ( (arr[i]) %5 === 0 ) ) {
+                        $("body .fizzbuzz_3").append("FizzBuzz ");
+                  // write Fizz if divisible by 3
+                  } else if ( (arr[i]) %3 === 0 ) {
+                        $("body .fizzbuzz_3").append("Fizz ");
+                  // write Buzz if divisble by 5
+                  } else if ( (arr[i]) %5 === 0 ) {
+                        $("body .fizzbuzz_3").append("Buzz ");
+                  // otherwise write i
+                  } else {
+                        $("body .fizzbuzz_3").append(" " + arr[i] + " ");
+                  }
+
+            } // end of for loop
+
+        } else {
+
+            alert("No element in array");
+            console.log("Error: No elements in array");
+        }
 
         $("body .fizzbuzz_3").append("</p><br>");
 
-    }// end of fizzbuzz_3                
+    }// end of fizzbuzz_3   
+
+    //same function above except use parameter of object specifying the word foo and bar
+    function fizzbuzz_4(obj) {
+
+        $("body .fizzbuzz_4").append("<p>FizzBuzz_4</p>");
+        $("body .fizzbuzz_4").append("<p>");
+            
+        for (i = 1; i <= 100; i++) {
+              // write foobar if divisible by 3 and 5  
+              if ((i%3 === 0) && (i%5 === 0)) {
+                    $("body .fizzbuzz_4").append(obj.divisibleByThree + obj.divisibleByFive + " ");
+              // write foo if divisible by 3
+              } else if (i%3 === 0) {
+                    $("body .fizzbuzz_4").append(obj.divisibleByThree + " ");
+              // write bar if divisble by 5
+              } else if (i%5 === 0) {
+                    $("body .fizzbuzz_4").append(obj.divisibleByFive + " ");
+              // otherwise write i
+              } else {
+                    $("body .fizzbuzz_4").append(" " + i + " ");
+              }
+
+        } // end of for loop    
+            $("body .fizzbuzz_4").append("</p><br>");
+
+    }// end of fizzbuzz_4  
+    
 
 
 
@@ -100,6 +137,8 @@ var main = function() {
     fizzbuzz_2(200, 300);
     // calling same function with parameter of array
     fizzbuzz_3([101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115]);
+    // calling same function with parameter of object
+    fizzbuzz_4({divisibleByThree: "foo", divisibleByFive: "bar"});
 };
 $(document).ready(main);
 
